1cb0a406bd9edc5ebad4d3131b5e7d334bf3aedf
RidwanSiddique44 <srsiddique118@gmail.com>
2022-05-05 14:27:09 +0600

    delete api added

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
